a394f10785a6ea1728bf5a3967112b5db070d9bc
Nghia <mxnghia49@gmail.com>
2020-06-22 12:52:41 +0700

    something

--- datacenter.php
+++ datacenter.php
@@ -46,22 +46,6 @@
                     <li>Customer_id:</li><li><input type="text" name="customer_id" /></li>
                     <li><input type="submit" /></li>
                 </form>
-                <?php
-                $sql = "INSERT INTO repceipt(receipt_id, product_id, seller_id, customer_id) VALUES ('$_POST[receipt_id]','$_POST[product_id]','$_POST[seller_id]', '$_POST[customer_id]')";
-                $stmt = $pdo->prepare($sql);
-                
-                 if (is_null($_POST[id])) {
-                   alert "ID must be not null";
-                 }
-                 else
-                 {
-                    if($stmt->execute() == TRUE){
-                        alert "Record inserted successfully.";
-                    } else {
-                        alert "Error inserting record: ";
-                    }
-                 }
-                ?>
             </ul>
                 <a href="#"><b>Thêm DL</b></a>
             </div>
